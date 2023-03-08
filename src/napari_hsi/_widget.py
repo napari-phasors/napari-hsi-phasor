44922bb97121f812164e3443c90ce3a955cb1084
schutyb <schutyb@schutyb.com>
2023-03-08 16:15:38 -0300

    widget

--- src/napari_hsi/_widget.py
+++ src/napari_hsi/_widget.py
@@ -10,6 +10,7 @@ from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+import numpy as np
 
 if TYPE_CHECKING:
     import napari
@@ -32,15 +33,3 @@ class ExampleQWidget(QWidget):
 
     def _on_click(self):
         print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
