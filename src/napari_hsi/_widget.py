cc40164a04ac01c08ef44b3c06bca81a5674c1a2
schutyb <schutyb@schutyb.com>
2023-04-27 12:44:58 -0300

    create phasor widget

--- src/napari_hsi/_widget.py
+++ src/napari_hsi/_widget.py
@@ -7,7 +7,6 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 Replace code below according to your needs.
 """
 from typing import TYPE_CHECKING
-
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 import numpy as np
@@ -16,20 +15,17 @@ if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+@magic_factory()
+def phasor(image_stack: "napari.layers.Image", harmonic: int) -> "napari.types.LayerDataTuple":
+    if isinstance(harmonic, int) and 0 < harmonic < len(image_stack):
+        data = np.fft.fft(image_stack, axis=0, norm='ortho')
+        dc = data[0].real
+        dc = np.where(dc != 0, dc, int(np.mean(dc, dtype=np.float64)))
+        g = data[harmonic].real
+        g /= dc
+        s = data[harmonic].imag
+        s /= -dc
+        md = np.sqrt(g ** 2 + s ** 2)
+        ph = np.angle(data[harmonic], deg=True)
+        avg = np.mean(image_stack, axis=0, dtype=np.float64)
+        return np.asarray([avg, g, s, md, ph])
